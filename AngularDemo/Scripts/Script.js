13f9a884c585c93f185de87cba9337551c63df29
Uppili Pranavane <up.pranavane@gmail.com>
2017-07-29 03:57:38 +0530

    commit with two way data binding  "ng-model"

--- AngularDemo/Scripts/Script.js
+++ AngularDemo/Scripts/Script.js
@@ -22,16 +22,17 @@
 //                });
 
 
-var app = angular.module("MyModule", []);
+var app = angular.module("MyModule", []);         //declearing the module MyModule to the app 
 
 
 
-app.controller ("MyController",function($scope)
+app.controller("MyController", function ($scope)   //app.controller function is used to link the controller with the module in the app, 
+                                                   //the controller name is MyController and the defeniotion of the function goes after function($scope)
                 
                 {
-                    var employees =
+                    var employees =                                                                                         // creating a variable named employee which is a string
                         [
-                            { name: "Uppili", Designation:"Software Engineer", Salary:12000, DateOfJoining:"June 14, 2017"},
+                            { name: "Uppili", Designation:"Software Engineer", Salary:12000, DateOfJoining:"June 14, 2017"},         //contents of the string in a key value pair
                             { name: "Tamil", Designation:"Software Tester", Salary:13000, DateOfJoining:"June 14, 2013"},
                             { name: "Nitai", Designation:"Software Analyst", Salary:14000, DateOfJoining:"June 14, 2019"},
                             { name: "Saravanan", Designation:"Software assigner", Salary:15000, DateOfJoining:"June 14, 2007"},
@@ -40,15 +41,15 @@ app.controller ("MyController",function($scope)
                         ];
                             
                
-$scope.employees = employees;
+$scope.employees = employees;       //since the variable decleared is private making it public by sending it to the $scope.employee 
                             
                 });
 
-app.controller ("MyController1",function($scope) {
-    var countries =
+app.controller ("MyController1",function($scope) {            //registering another controlle MyController1 to the app
+    var countries =                                           // to demontrate the array within a array                          
         [
            { name : "India" ,
-           City:
+           City:                                             //there is an array with the key city in the array name   
             [
                {name: "delhi"},
                 {name: "Mumbai"},
@@ -67,5 +68,13 @@ app.controller ("MyController1",function($scope) {
 
            }
         ]
-    $scope.countries = countries;
+    $scope.countries = countries;   // making the countries array as public
+});
+
+app.controller("MyController2", function ($scope) {
+    var employees = {
+
+        name: "Uppili", designation: "Senior Engineer", date: "June 14, 2017", salary: 500000
+    };
+    $scope.employees = employees;    
 });
