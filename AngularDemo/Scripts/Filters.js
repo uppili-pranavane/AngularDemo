0217d7664616fb9d0dded973faf7571b02011dad
Uppili Pranavane <up.pranavane@gmail.com>
2017-07-30 16:48:42 +0530

    implemented custom filter Filter.js

---
+++ AngularDemo/Scripts/Filters.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="Script.js" />
+
+app.filter("gender", function () {
+    return function (gender)
+    {
+        switch(gender)
+        {
+            case 1:
+                return "Male";
+            case 2:
+                return "Female";
+            case 3:
+                return "Not Disclosed";
+        }
+}
+})
